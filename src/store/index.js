373eb3bea1af47539061bb9c85c307cf593e0df1
Julian Mansueto <julians.mg@gmail.com>
2018-08-12 20:22:44 -0300

    prettified

--- src/store/index.js
+++ src/store/index.js
@@ -1,29 +1,29 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import getters from './getters'
+import Vue from "vue";
+import Vuex from "vuex";
+import getters from "./getters";
 import {
   getUserProfile,
   isAccessGranted,
   getDiscussions,
   getDiscussionDetails
-} from '../easy-steem'
-import { handleProfile } from '../easy-steem/steem-connect'
-import { handleDiscussions, renderMd } from '../easy-steem/steemd'
+} from "../easy-steem";
+import { handleProfile } from "../easy-steem/steem-connect";
+import { handleDiscussions, renderMd } from "../easy-steem/steemd";
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
-    filter: 'trending',
+    filter: "trending",
     filters: [
-      { value: 'trending', label: 'Trending' },
-      { value: 'hot', label: 'Hot' },
-      { value: 'new', label: 'New' }
+      { value: "trending", label: "Trending" },
+      { value: "hot", label: "Hot" },
+      { value: "new", label: "New" }
     ],
     tags: [],
     alert: false,
-    alertMessage: 'A error has occured. Please try again later.',
-    tag: '',
+    alertMessage: "A error has occured. Please try again later.",
+    tag: "",
     posts: [],
     loggedIn: isAccessGranted(),
     profile: {},
@@ -32,44 +32,50 @@ export const store = new Vuex.Store({
   },
   mutations: {
     toggleDrawer(state) {
-    	state.drawer = !state.drawer
+      state.drawer = !state.drawer;
     },
     getUserProfile(state) {
-      getUserProfile().then(res => {
-        state.profile = handleProfile(res)
-      }).catch(err =>{
-        console.log('Error while getting user Profile')
-        console.log(err)
-        state.alert = true
-      })
+      getUserProfile()
+        .then(res => {
+          state.profile = handleProfile(res);
+        })
+        .catch(err => {
+          console.log("Error while getting user Profile");
+          console.log(err);
+          state.alert = true;
+        });
     },
     getDiscussions(state) {
-      const { filter, tag, postsLimit } = state
-      getDiscussions({ filter, query: { tag, limit: postsLimit } }).then(res => {
-        state.posts = handleDiscussions(res)
-      }).catch(err => {
-        console.log('Error while getting discussions')
-        console.log(err)
-        state.alert = true
-      })
+      const { filter, tag, postsLimit } = state;
+      getDiscussions({ filter, query: { tag, limit: postsLimit } })
+        .then(res => {
+          state.posts = handleDiscussions(res);
+        })
+        .catch(err => {
+          console.log("Error while getting discussions");
+          console.log(err);
+          state.alert = true;
+        });
     },
     getDiscussionDetails(state, payload) {
-      getDiscussionDetails(payload).then(res => {
-        state.currentPost = {
-          title: res.title,
-          content: renderMd(res.body)
-        }
-      }).catch(err => console.log(err))
+      getDiscussionDetails(payload)
+        .then(res => {
+          state.currentPost = {
+            title: res.title,
+            content: renderMd(res.body)
+          };
+        })
+        .catch(err => console.log(err));
     }
   },
   actions: {
     init({ commit }) {
-      commit('getUserProfile')
-      commit('getDiscussions')
+      commit("getUserProfile");
+      commit("getDiscussions");
     },
     getDiscussionDetails({ commit }, payload) {
-      commit('getDiscussionDetails', payload)
+      commit("getDiscussionDetails", payload);
     }
   },
   getters
-})
+});
