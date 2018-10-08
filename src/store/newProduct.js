f3a217ed3f4fb73fef63aadcb26e98142fa5f7a9
julianmnst <julians.mg@gmail.com>
2018-10-07 21:24:47 -0300

    all values default to 0

--- src/store/newProduct.js
+++ src/store/newProduct.js
@@ -3,22 +3,15 @@ import api from '../api';
 export let newProduct = {
   namespaced: true,
 	state: {
-    name: 'Ethereum Mug',
-    author: 'julianmnst',
-    permlink: 'eth-mug654682',
-    pending_payout_value: '211.630 SBD',
-    price: '10.000 SBD',
-    image: 'https://rlv.zcache.com/ethereum_mug-r0703ea79845841799e1cbc587e18dd03_kz9an_540.jpg?rlvnet=1',
-    type: 'direct-sell',
+    name: '',
+    author: '',
+    permlink: '',
+    price: ' SBD',
+    type: null,
     wishlist: false,
-    description: 'Not too long description',
-    pictures: [
-      'https://rlv.zcache.com/ethereum_mug-r0703ea79845841799e1cbc587e18dd03_kz9an_540.jpg?rlvnet=1',
-      'https://steemitimages.com/0x0/http://puu.sh/qj7LB/1bfd81aa0c.jpg',
-      'https://hoodl.me/wp-content/uploads/2017/12/bitcoin-logo-mug.jpg',
-      'https://hodlmonkey.com/wp-content/uploads/2017/11/mockup-f11bb83c.jpg'
-    ],
-    stock: 1,
+    description: '',
+    pictures: [''],
+    stock: 0,
     category: null
   },
   mutations: {
@@ -31,9 +24,9 @@ export let newProduct = {
     removePicture(state, payload) {
       state.pictures.splice(payload, 1)
     },
-    createProduct(state) {
+    createProduct(state, payload) {
       console.log(state);
-      api().post('/app/product/add', state)
+      api().post('/app/product/add', { ...state, author: payload.username })
         .then(res => console.log(res))
         .catch(err => console.log(err))
     }
@@ -48,8 +41,9 @@ export let newProduct = {
     removePicture({ commit }, payload) {
       commit('removePicture', payload)
     },
-    createProduct({ commit }) {
-      commit('createProduct')
+    createProduct({ commit, rootState }) {
+      console.log(rootState.profile.username)
+      commit('createProduct', rootState)
     }
   }
 }
